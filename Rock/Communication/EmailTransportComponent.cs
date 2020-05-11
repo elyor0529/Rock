34979f7ba4405ecb809db974343a2ec9c412cb83
Matt Bronkema <matt.bronkema@sparkdevnetwork.org>
2020-05-11 09:54:48 -0400

    - Adding comments.

--- Rock/Communication/EmailTransportComponent.cs
+++ Rock/Communication/EmailTransportComponent.cs
@@ -1,12 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Data.Entity;
-using System.IO;
 using System.Linq;
-using System.Net;
 using System.Net.Mail;
 using System.Text.RegularExpressions;
-using Rock.Attribute;
 using Rock.Communication.Transport;
 using Rock.Data;
 using Rock.Model;
@@ -15,10 +12,28 @@ using Rock.Web.Cache;
 
 namespace Rock.Communication
 {
+    /// <summary>
+    /// This abstract class implements the code needed to create an email with all of the validation, lava substitution, and error checking completed.
+    /// </summary>
+    /// <seealso cref="Rock.Communication.TransportComponent" />
     public abstract class EmailTransportComponent : TransportComponent
     {
+        /// <summary>
+        /// Send the implementation specific email. This class will call this method and pass the post processed data in a  rock email message which
+        /// can then be used to send the implementation specific message.
+        /// </summary>
+        /// <param name="rockEmailMessage">The rock email message.</param>
+        /// <returns></returns>
         protected abstract EmailSendResponse SendEmail( RockEmailMessage rockEmailMessage );
 
+        /// <summary>
+        /// Sends the specified rock message.
+        /// </summary>
+        /// <param name="rockMessage">The rock message.</param>
+        /// <param name="mediumEntityTypeId">The medium entity type identifier.</param>
+        /// <param name="mediumAttributes">The medium attributes.</param>
+        /// <param name="errorMessages">The error messages.</param>
+        /// <returns></returns>
         public override bool Send( RockMessage rockMessage, int mediumEntityTypeId, Dictionary<string, string> mediumAttributes, out List<string> errorMessages )
         {
             errorMessages = new List<string>();
@@ -68,6 +83,12 @@ namespace Rock.Communication
             return !errorMessages.Any();
         }
 
+        /// <summary>
+        /// Sends the specified communication.
+        /// </summary>
+        /// <param name="communication">The communication.</param>
+        /// <param name="mediumEntityTypeId">The medium entity type identifier.</param>
+        /// <param name="mediumAttributes">The medium attributes.</param>
         public override void Send( Model.Communication communication, int mediumEntityTypeId, Dictionary<string, string> mediumAttributes )
         {
             using ( var communicationRockContext = new RockContext() )
@@ -187,6 +208,181 @@ namespace Rock.Communication
             }
         }
 
+        /// <summary>
+        /// Validates the recipient.
+        /// </summary>
+        /// <param name="recipient">The recipient.</param>
+        /// <param name="isBulkCommunication">if set to <c>true</c> [is bulk communication].</param>
+        /// <returns></returns>
+        public override bool ValidRecipient( CommunicationRecipient recipient, bool isBulkCommunication )
+        {
+            bool valid = base.ValidRecipient( recipient, isBulkCommunication );
+            if ( valid )
+            {
+                var person = recipient?.PersonAlias?.Person;
+                if ( person != null )
+                {
+                    if ( string.IsNullOrWhiteSpace( person.Email ) )
+                    {
+                        recipient.Status = CommunicationRecipientStatus.Failed;
+                        recipient.StatusNote = "No Email Address";
+                        valid = false;
+                    }
+                    else if ( !person.IsEmailActive )
+                    {
+                        recipient.Status = CommunicationRecipientStatus.Failed;
+                        recipient.StatusNote = "Recipient Email Address is not active";
+                        valid = false;
+                    }
+                    else if ( person.EmailPreference == Model.EmailPreference.DoNotEmail )
+                    {
+                        recipient.Status = CommunicationRecipientStatus.Failed;
+                        recipient.StatusNote = "Communication Preference of 'Do Not Send Communication'";
+                        valid = false;
+                    }
+                    else if ( person.EmailPreference == Model.EmailPreference.NoMassEmails && isBulkCommunication )
+                    {
+                        recipient.Status = CommunicationRecipientStatus.Failed;
+                        recipient.StatusNote = "Communication Preference of 'No Bulk Communication'";
+                        valid = false;
+                    }
+                }
+            }
+
+            return valid;
+        }
+                
+        /// <summary>
+        /// Gets the list of safe domains from COMMUNICATION_SAFE_SENDER_DOMAINS.
+        /// </summary>
+        /// <returns></returns>
+        protected List<string> GetSafeDomains()
+        {
+            // Get the safe sender domains
+            var safeDomainValues = DefinedTypeCache.Get( SystemGuid.DefinedType.COMMUNICATION_SAFE_SENDER_DOMAINS.AsGuid() ).DefinedValues;
+            return safeDomainValues.Select( v => v.Value ).ToList();
+        }
+
+        /// <summary>
+        /// Gets the domain part of the email address everything after the @.
+        /// </summary>
+        /// <param name="emailAddress">The email address.</param>
+        /// <returns></returns>
+        protected string GetEmailDomain( string emailAddress )
+        {
+            var fromParts = emailAddress.Split( new char[] { '@' }, StringSplitOptions.RemoveEmptyEntries );
+            if ( fromParts.Length == 2 )
+            {
+                return fromParts[1];
+            }
+            return "";
+        }
+
+        /// <summary>
+        /// Checks the safe sender.
+        /// </summary>
+        /// <param name="toEmailAddresses">To email addresses.</param>
+        /// <param name="fromEmail">From email.</param>
+        /// <param name="organizationEmail">The organization email.</param>
+        /// <returns></returns>
+        protected virtual SafeSenderResult CheckSafeSender( List<string> toEmailAddresses, MailAddress fromEmail, string organizationEmail )
+        {
+            var result = new SafeSenderResult();
+
+            // Get the safe sender domains
+            var safeDomains = GetSafeDomains();
+
+            // Check to make sure the From email domain is a safe sender, if so then we are done.
+            var emailDomain = GetEmailDomain( fromEmail.Address );
+            if ( emailDomain.IsNotNullOrWhiteSpace() && safeDomains.Contains( emailDomain, StringComparer.OrdinalIgnoreCase ) )
+            {
+                return result;
+            }
+
+            // The sender domain is not considered safe so check all the recipients to see if they have a domain that does not requrie a safe sender
+            var safeDomainValues = DefinedTypeCache.Get( SystemGuid.DefinedType.COMMUNICATION_SAFE_SENDER_DOMAINS.AsGuid() ).DefinedValues;
+            foreach ( var toEmailAddress in toEmailAddresses )
+            {
+                bool safe = false;
+                var toEmailDomain = GetEmailDomain( toEmailAddress );
+                if ( toEmailDomain.IsNotNullOrWhiteSpace() && safeDomains.Contains( toEmailDomain, StringComparer.OrdinalIgnoreCase ) )
+                {
+                    var domain = safeDomainValues.FirstOrDefault( dv => dv.Value.Equals( toEmailDomain, StringComparison.OrdinalIgnoreCase ) );
+                    safe = domain != null && domain.GetAttributeValue( "SafeToSendTo" ).AsBoolean();
+                }
+
+                if ( !safe )
+                {
+                    result.IsUnsafeDomain = true;
+                    break;
+                }
+            }
+
+            if ( result.IsUnsafeDomain )
+            {
+                if ( !string.IsNullOrWhiteSpace( organizationEmail ) && !organizationEmail.Equals( fromEmail.Address, StringComparison.OrdinalIgnoreCase ) )
+                {
+                    result.SafeFromAddress = new MailAddress( organizationEmail, fromEmail.DisplayName );
+                    result.ReplyToAddress = fromEmail;
+                }
+            }
+
+            return result;
+        }
+
+        private string GetFromName( RockEmailMessage emailMessage, Dictionary<string, object> mergeFields, GlobalAttributesCache globalAttributes )
+        {
+            string fromName = emailMessage.FromName.ResolveMergeFields( mergeFields, emailMessage.CurrentPerson, emailMessage.EnabledLavaCommands );
+            fromName = fromName.IsNullOrWhiteSpace() ? globalAttributes.GetValue( "OrganizationName" ) : fromName;
+            return fromName;
+        }
+
+        private string GetFromAddress( RockEmailMessage emailMessage, Dictionary<string, object> mergeFields, GlobalAttributesCache globalAttributes )
+        {
+
+            // Resolve any possible merge fields in the from address
+            string fromAddress = emailMessage.FromEmail.ResolveMergeFields( mergeFields, emailMessage.CurrentPerson, emailMessage.EnabledLavaCommands );
+            fromAddress = fromAddress.IsNullOrWhiteSpace() ? globalAttributes.GetValue( "OrganizationEmail" ) : fromAddress;
+            return fromAddress;
+        }
+
+        private Dictionary<string, object> GetAllMergeFields( Person currentPerson, Dictionary<string, object> additionalMergeFields )
+        {
+            // Common Merge Field
+            var mergeFields = Lava.LavaHelper.GetCommonMergeFields( null, currentPerson );
+
+            if ( additionalMergeFields != null )
+            {
+                foreach ( var mergeField in additionalMergeFields )
+                {
+                    mergeFields.AddOrReplace( mergeField.Key, mergeField.Value );
+                }
+            }
+
+            return mergeFields;
+        }
+
+        private string GetRecipientReplyToAddress( RockEmailMessage emailMessage, Dictionary<string, object> mergeFields, SafeSenderResult safeSenderResult )
+        {
+            var replyToAddress = string.Empty;
+            if ( emailMessage.ReplyToEmail.IsNotNullOrWhiteSpace() )
+            {
+                replyToAddress = emailMessage.ReplyToEmail.ResolveMergeFields( mergeFields, emailMessage.CurrentPerson, emailMessage.EnabledLavaCommands );
+            }
+
+            if ( safeSenderResult.IsUnsafeDomain && safeSenderResult.ReplyToAddress != null )
+            {
+                if ( replyToAddress.IsNullOrWhiteSpace() )
+                {
+                    replyToAddress = safeSenderResult.ReplyToAddress.ToString();
+                }
+                else
+                {
+                    replyToAddress += $",{safeSenderResult.ReplyToAddress.ToString()}";
+                }
+            }
+            return replyToAddress;
+        }
 
         private RockEmailMessage GetTemplateRockEmailMessage( RockMessage rockMessage, Dictionary<string, object> mergeFields, GlobalAttributesCache globalAttributes )
         {
@@ -414,7 +610,7 @@ namespace Rock.Communication
 
             // Reply To
             recipientEmail.ReplyToEmail = GetRecipientReplyToAddress( emailMessage, mergeFields, checkResult );
-            
+
             // From
             if ( checkResult.IsUnsafeDomain && checkResult.SafeFromAddress != null )
             {
@@ -497,164 +693,5 @@ namespace Rock.Communication
             return recipientEmail;
         }
 
-        protected List<string> GetSafeDomains()
-        {
-            // Get the safe sender domains
-            var safeDomainValues = DefinedTypeCache.Get( SystemGuid.DefinedType.COMMUNICATION_SAFE_SENDER_DOMAINS.AsGuid() ).DefinedValues;
-            return safeDomainValues.Select( v => v.Value ).ToList();
-        }
-
-        protected string GetEmailDomain( string emailAddress )
-        {
-            var fromParts = emailAddress.Split( new char[] { '@' }, StringSplitOptions.RemoveEmptyEntries );
-            if ( fromParts.Length == 2 )
-            {
-                return fromParts[1];
-            }
-            return "";
-        }
-
-        protected virtual SafeSenderResult CheckSafeSender( List<string> toEmailAddresses, MailAddress fromEmail, string organizationEmail )
-        {
-            var result = new SafeSenderResult();
-
-            // Get the safe sender domains
-            var safeDomains = GetSafeDomains();
-
-            // Check to make sure the From email domain is a safe sender, if so then we are done.
-            var emailDomain = GetEmailDomain( fromEmail.Address );
-            if ( emailDomain.IsNotNullOrWhiteSpace() && safeDomains.Contains( emailDomain, StringComparer.OrdinalIgnoreCase ) )
-            {
-                return result;
-            }
-
-            // The sender domain is not considered safe so check all the recipients to see if they have a domain that does not requrie a safe sender
-            var safeDomainValues = DefinedTypeCache.Get( SystemGuid.DefinedType.COMMUNICATION_SAFE_SENDER_DOMAINS.AsGuid() ).DefinedValues;
-            foreach ( var toEmailAddress in toEmailAddresses )
-            {
-                bool safe = false;
-                var toEmailDomain = GetEmailDomain( toEmailAddress );
-                if ( toEmailDomain.IsNotNullOrWhiteSpace() && safeDomains.Contains( toEmailDomain, StringComparer.OrdinalIgnoreCase ) )
-                {
-                    var domain = safeDomainValues.FirstOrDefault( dv => dv.Value.Equals( toEmailDomain, StringComparison.OrdinalIgnoreCase ) );
-                    safe = domain != null && domain.GetAttributeValue( "SafeToSendTo" ).AsBoolean();
-                }
-
-                if ( !safe )
-                {
-                    result.IsUnsafeDomain = true;
-                    break;
-                }
-            }
-
-            if ( result.IsUnsafeDomain )
-            {
-                if ( !string.IsNullOrWhiteSpace( organizationEmail ) && !organizationEmail.Equals( fromEmail.Address, StringComparison.OrdinalIgnoreCase ) )
-                {
-                    result.SafeFromAddress = new MailAddress( organizationEmail, fromEmail.DisplayName );
-                    result.ReplyToAddress = fromEmail;
-                }
-            }
-
-            return result;
-        }
-
-        private string GetFromName( RockEmailMessage emailMessage, Dictionary<string, object> mergeFields, GlobalAttributesCache globalAttributes )
-        {
-            string fromName = emailMessage.FromName.ResolveMergeFields( mergeFields, emailMessage.CurrentPerson, emailMessage.EnabledLavaCommands );
-            fromName = fromName.IsNullOrWhiteSpace() ? globalAttributes.GetValue( "OrganizationName" ) : fromName;
-            return fromName;
-        }
-
-        private string GetFromAddress( RockEmailMessage emailMessage, Dictionary<string, object> mergeFields, GlobalAttributesCache globalAttributes )
-        {
-
-            // Resolve any possible merge fields in the from address
-            string fromAddress = emailMessage.FromEmail.ResolveMergeFields( mergeFields, emailMessage.CurrentPerson, emailMessage.EnabledLavaCommands );
-            fromAddress = fromAddress.IsNullOrWhiteSpace() ? globalAttributes.GetValue( "OrganizationEmail" ) : fromAddress;
-            return fromAddress;
-        }
-
-        private Dictionary<string, object> GetAllMergeFields( Person currentPerson, Dictionary<string, object> additionalMergeFields )
-        {
-            // Common Merge Field
-            var mergeFields = Lava.LavaHelper.GetCommonMergeFields( null, currentPerson );
-
-            if ( additionalMergeFields != null )
-            {
-                foreach ( var mergeField in additionalMergeFields )
-                {
-                    mergeFields.AddOrReplace( mergeField.Key, mergeField.Value );
-                }
-            }
-
-            return mergeFields;
-        }
-
-        private string GetRecipientReplyToAddress(RockEmailMessage emailMessage, Dictionary<string, object> mergeFields, SafeSenderResult safeSenderResult )
-        {
-            var replyToAddress = string.Empty;
-            if ( emailMessage.ReplyToEmail.IsNotNullOrWhiteSpace() )
-            {
-                replyToAddress = emailMessage.ReplyToEmail.ResolveMergeFields( mergeFields, emailMessage.CurrentPerson, emailMessage.EnabledLavaCommands );
-            }
-
-            if ( safeSenderResult.IsUnsafeDomain && safeSenderResult.ReplyToAddress != null )
-            {
-                if ( replyToAddress.IsNullOrWhiteSpace() )
-                {
-                    replyToAddress = safeSenderResult.ReplyToAddress.ToString();
-                }
-                else
-                {
-                    replyToAddress += $",{safeSenderResult.ReplyToAddress.ToString()}";
-                }
-            }
-            return replyToAddress;
-        }
-
-        /// <summary>
-        /// Validates the recipient.
-        /// </summary>
-        /// <param name="recipient">The recipient.</param>
-        /// <param name="isBulkCommunication">if set to <c>true</c> [is bulk communication].</param>
-        /// <returns></returns>
-        public override bool ValidRecipient( CommunicationRecipient recipient, bool isBulkCommunication )
-        {
-            bool valid = base.ValidRecipient( recipient, isBulkCommunication );
-            if ( valid )
-            {
-                var person = recipient?.PersonAlias?.Person;
-                if ( person != null )
-                {
-                    if ( string.IsNullOrWhiteSpace( person.Email ) )
-                    {
-                        recipient.Status = CommunicationRecipientStatus.Failed;
-                        recipient.StatusNote = "No Email Address";
-                        valid = false;
-                    }
-                    else if ( !person.IsEmailActive )
-                    {
-                        recipient.Status = CommunicationRecipientStatus.Failed;
-                        recipient.StatusNote = "Recipient Email Address is not active";
-                        valid = false;
-                    }
-                    else if ( person.EmailPreference == Model.EmailPreference.DoNotEmail )
-                    {
-                        recipient.Status = CommunicationRecipientStatus.Failed;
-                        recipient.StatusNote = "Communication Preference of 'Do Not Send Communication'";
-                        valid = false;
-                    }
-                    else if ( person.EmailPreference == Model.EmailPreference.NoMassEmails && isBulkCommunication )
-                    {
-                        recipient.Status = CommunicationRecipientStatus.Failed;
-                        recipient.StatusNote = "Communication Preference of 'No Bulk Communication'";
-                        valid = false;
-                    }
-                }
-            }
-
-            return valid;
-        }
     }
 }
